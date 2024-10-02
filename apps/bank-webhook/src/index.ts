b49a471124aed45d94a8624bf112cfad80b2916e
Amar Prasad <amarprasad77354@gmail.com>
2024-10-02 22:03:13 +0530

    changed schema, added webhook backend

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import db from '@repo/db/client'
+
+const app = express();
+
+
+app.post("/hdfcWebhook", async (req, res) => {
+    // TODO: Add zod validation here
+    // Check if this request actually came from hdfc back, use a webhook secret here
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+    try {
+        // Update balance in database, add transaction
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                data: {
+                    amount: {
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+            // Adding the trns. in onRampTransactions
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ]);
+        res.status(200).json({
+            message: "captured"
+        })
+    } catch (error) {
+         // successfully captured req.
+        res.status(200).json({
+            message: "failed"
+        })
+    }
+})
+
